8c36364938419c8ea5fda98d2d63cddc4ef10c42
tianxu <1830015699@qq.com>
2022-11-12 13:03:44 +0800

    联系人列表页样式修改

--- chat-ui/src/pages/MailList/index.tsx
+++ chat-ui/src/pages/MailList/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {TextMessage, DocDetail, PhoneCall} from "@icon-park/react";
 import styles from './index.module.less';
 
 import Avatar from '../../components/Avatar';
@@ -12,12 +13,24 @@ const Index = () => {
       </div>
       <div className={styles.mailBody}>
         <section className={styles.mailItem}>
-          <Avatar src={face1} size={"60px"} status={"online"} statusIconSize={"10px"} />
-          <div className={styles.mailName}>当年哇哦难道我noon味道</div>
+          <div className={styles.mailInfo}>
+            <Avatar src={face1} size={"60px"} status={"online"} statusIconSize={"10px"} />
+            <div className={styles.mailName}>当年哇哦难道我noon味道</div>
+          </div>
+          <div className={styles.mailAction}>
+            <TextMessage theme="outline" size="20" fill="#333" strokeWidth={3} strokeLinecap="square"/>
+            <PhoneCall theme="outline" size="20" fill="#333" strokeWidth={3} strokeLinecap="square"/>
+          </div>
         </section>
         <section className={styles.mailItem}>
-          <Avatar src={face1} size={"60px"} status={"offline"} statusIconSize={"10px"} />
-          <div className={styles.mailName}>789</div>
+          <div className={styles.mailInfo}>
+            <Avatar src={face1} size={"60px"} status={"offline"} statusIconSize={"10px"} />
+            <div className={styles.mailName}>当年哇哦难道我noon味道</div>
+          </div>
+          <div className={styles.mailAction}>
+            <TextMessage theme="outline" size="20" fill="#333" strokeWidth={3} strokeLinecap="square"/>
+            <PhoneCall theme="outline" size="20" fill="#333" strokeWidth={3} strokeLinecap="square"/>
+          </div>
         </section>
       </div>
 
