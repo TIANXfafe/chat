7657b06fca626087db42a17829d3cba510a0a4b5
田旭 <1830015699@qq.com>
2022-11-14 14:23:09 +0800

    联系人页面增加动画

--- chat-ui/src/pages/MailList/index.tsx
+++ chat-ui/src/pages/MailList/index.tsx
@@ -1,40 +1,99 @@
 import React from 'react';
-import {TextMessage, DocDetail, PhoneCall} from "@icon-park/react";
-import styles from './index.module.less';
+import {TextMessage, PhoneCall} from "@icon-park/react";
+import {config, animated, useChain, useSpring, useSpringRef, useTransition} from "react-spring";
 
+import styles from './index.module.less';
 import Avatar from '../../components/Avatar';
 import face1 from '../../assets/images/face-male-1.jpg';
 
+const data = [
+  {
+    id: 1,
+    name: 'nfonofaoawdf',
+    avatar: face1
+  },
+  {
+    id: 2,
+    name: 'nfonofaoawdf',
+    avatar: face1
+  },
+  {
+    id: 3,
+    name: 'nfonofaoawdf',
+    avatar: face1
+  },
+  {
+    id: 4,
+    name: 'nfonofaoawdf',
+    avatar: face1
+  },
+  {
+    id: 5,
+    name: 'nfonofaoawdf',
+    avatar: face1
+  },
+  {
+    id: 6,
+    name: 'nfonofaoawdf',
+    avatar: face1
+  }
+]
+
 const Index = () => {
+  const springApi = useSpringRef();
+  const style = useSpring({
+    ref: springApi,
+    config: config.stiff,
+    from: {
+      opacity: 0,
+      transform: 'scale(0.2)'
+    },
+    to: {
+      opacity: 1,
+      transform: 'scale(1)'
+    }
+  });
+
+  const transApi = useSpringRef();
+  const transition = useTransition(data, {
+    ref: transApi,
+    trail: 400 / data.length,
+    from: {
+      opacity: 0,
+      scale: 0
+    },
+    enter: {
+      opacity: 1,
+      scale: 1
+    }
+  });
+
+
+  useChain([springApi, transApi], [0, 0.1]);
+
   return (
-    <div className={styles.mailList}>
-      <div className={styles.mailHeader}>
+    <animated.div className={styles.mailList} style={style}>
+      <div className={styles.mailHeader} >
         123
       </div>
-      <div className={styles.mailBody}>
-        <section className={styles.mailItem}>
-          <div className={styles.mailInfo}>
-            <Avatar src={face1} size={"60px"} status={"online"} statusIconSize={"10px"} />
-            <div className={styles.mailName}>当年哇哦难道我noon味道</div>
-          </div>
-          <div className={styles.mailAction}>
-            <TextMessage theme="outline" size="20" fill="#333" strokeWidth={3} strokeLinecap="square"/>
-            <PhoneCall theme="outline" size="20" fill="#333" strokeWidth={3} strokeLinecap="square"/>
-          </div>
-        </section>
-        <section className={styles.mailItem}>
-          <div className={styles.mailInfo}>
-            <Avatar src={face1} size={"60px"} status={"offline"} statusIconSize={"10px"} />
-            <div className={styles.mailName}>当年哇哦难道我noon味道</div>
-          </div>
-          <div className={styles.mailAction}>
-            <TextMessage theme="outline" size="20" fill="#333" strokeWidth={3} strokeLinecap="square"/>
-            <PhoneCall theme="outline" size="20" fill="#333" strokeWidth={3} strokeLinecap="square"/>
-          </div>
-        </section>
+      <div className={styles.mailBody} >
+        {transition((style: any, item: any) => (
+          <animated.div
+            className={styles.mailItem}
+            style={style}
+          >
+            <div className={styles.mailInfo}>
+              <Avatar src={item.avatar} size={"60px"} status={"online"} statusIconSize={"10px"} />
+              <div className={styles.mailName}>{item.name}</div>
+            </div>
+            <div className={styles.mailAction}>
+              <TextMessage theme="outline" size="20" fill="#333" strokeWidth={3} strokeLinecap="square"/>
+              <PhoneCall theme="outline" size="20" fill="#333" strokeWidth={3} strokeLinecap="square"/>
+            </div>
+          </animated.div>
+        ))}
       </div>
-
-    </div>
+    </animated.div>
   );
 };
 
